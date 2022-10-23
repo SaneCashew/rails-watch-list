c13db9c9cccfa5e80bc5a19e26f0a4da79b85a97
Jonathan Troupe <sanetroupe@gmail.com>
2022-10-23 01:37:48 -0400

    Finished all of the basic required functions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,4 +3,16 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
   end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.create(comment: params[:bookmark][:comment], movie_id: @movie.id, list_id: @list.id)
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.delete
+  end
 end
